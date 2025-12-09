92f4d785525f8e491e95bf685645f466cb01842d
andrenautadev <andrelucas.alsdsbr1@gmail.com>
2025-12-09 15:01:49 -0300

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, NgModule, signal } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // <-- Necessárioadicionar essa importação
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule // Necessário para usar [(ngModel)]
+    FormsModule
   ],
 })
 export class AppModule { }
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // atenção: é "styleUrls" no plural
 })
 export class App {
   protected readonly title = signal('loja-jogos');
